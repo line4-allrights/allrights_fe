2de9b5a730050f197091d877809e4bb27fa8f8e4
sheepyis <zx9086@naver.com>
2023-11-05 01:10:10 +0900

    router settings

--- allrights_fe/src/router.jsx
+++ allrights_fe/src/router.jsx
@@ -2,6 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import React from "react";
 import App from "./App";
 import Mainpage from "./pages/mainpage/Mainpage";
+import Soundeffect from "./pages/soundeffect/Soundeffect";
+import BgMusic from "./pages/bgmusic/BgMusic";
+import Upload from "./pages/uploadpage/Upload";
+import Signin from "./pages/signIn/Signin";
+import Signup from "./pages/signUp/Signup";
+import Mypage from "./pages/mypage/Mypage";
+import NotFound from "./pages/notfound/Notfound";
 
 const router = createBrowserRouter([
   {
@@ -10,9 +17,34 @@ const router = createBrowserRouter([
     children: [
       {
         path: "",
-        element: <Mainpage />,
+        element: <Mainpage />
       },
+      {
+        path: "sound",
+        element: <Soundeffect />
+      },
+      {
+        path: "music",
+        element: <BgMusic />
+      },
+      {
+        path: "upload",
+        element: <Upload />
+      },
+      {
+        path: "signin",
+        element: <Signin />
+      },
+      {
+        path: "signup",
+        element: <Signup />
+      },      
+      {
+        path: "mypage",
+        element: <Mypage />
+      }
     ],
+    errorElement: <NotFound />
   },
 ]);
 
