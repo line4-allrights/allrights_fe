33be00a642f34502f28047dd6b27bf5ed8c93fff
sheepyis <zx9086@naver.com>
2023-11-07 03:42:40 +0900

    input 박스 수정 중

--- allrights_fe/src/components/input/input-search.jsx
+++ allrights_fe/src/components/input/input-search.jsx
@@ -1,11 +1,11 @@
 import {Home, HomeExplain,HomeExplainContainer, HomeP,HomeExP} from "../../pages/soundeffect/Soundeffect.jsx";
 import styled from "styled-components";
-import color from "../../styles/colors.js";
+import colors from "../../styles/colors.js";
 import ButtonMain from "../button/button-main.jsx";
 import { useState } from "react";
 
 const HomeBanner =styled.div`
-    background: ${color.mainDarkBlue};
+    background: ${colors.mainDarkBlue};
     width: 100%;
     margin-top:10vw;
     margin-bottom:10vw;
@@ -13,27 +13,26 @@ const HomeBanner =styled.div`
 
 const SearchContainer =styled.div`
     display:flex;
-    flex-direction: row;
     gap:0.8vw;
     align-items: center;
 
-    @media (max-width: 768px) {
-    gap: 4px; 
+    // @media (max-width: 768px) {
+    // gap: 4px; 
     
     
-    & > * {
-      flex-grow: 1; 
-      height: auto;
-    }
+    // & > * {
+    //   flex-grow: 1; 
+    //   height: auto;
+    // }
 }
 `
 
 const SearchInput = styled.input`
   width: 18.95vw;
   height: 2.2vw;
-  flex-shrink: 0;
+  // flex-shrink: 0;
   border-radius: 1.2vw;
-  border: 2px solid #E4E8EF; 
+  border: 0.1vw solid #E4E8EF; 
   padding: 0 15px; 
   align-items: center;
   color:#999;
@@ -69,25 +68,26 @@ const InputSearch=()=>{
 
     return (
         <Home>
-        <HomeBanner style={{backgroundColor:color.mainDarkBlue}}>
+        <HomeBanner style={{backgroundColor:colors.mainDarkBlue}}>
             <HomeExplain>
-                <HomeExplainContainer>
-            <HomeP style={{ fontSize: '1.75vw', marginBottom:'2vw'}} >
-                키워드로 검색해보세요 <br />
+              <HomeExplainContainer style={{ alignItems: "center", marginBottom: "2vw"}}>
+                <div style={{width: "0.5vw", height: "0.5vw", border: "none", borderRadius: "50%", backgroundColor: colors.white, marginBottom: "1vw"}}/>
+                <HomeP style={{ fontSize: '1.75vw', marginBottom:'2vw'}} >
+                  키워드로 검색해보세요 <br />
                 </HomeP> 
-            <HomeExP>
-            밝은 / 발랄한 / 깨끗한 / 청량한 / 경쾌한 / 어두운 / 오싹한 / 웅장한 / 급박한 <br/>
-                <span style={{textDecorationLine:'underline', lineHeight:'220%'}}>간단한 검색어로 원하는 자료를 찾아보세요!</span>
-            </HomeExP>
+                <HomeExP>
+                밝은 / 발랄한 / 깨끗한 / 청량한 / 경쾌한 / 어두운 / 오싹한 / 웅장한 / 급박한 <br/>
+                    <span style={{textDecorationLine:'underline', lineHeight:'220%'}}>간단한 검색어로 원하는 자료를 찾아보세요!</span>
+                </HomeExP>
             </HomeExplainContainer>
 
             <SearchContainer>
-            <SearchInput
-                placeholder="검색어를 입력하세요"
-                value={search} 
-                onChange={handleSearchInputChange}
-            />
-            <ButtonMain buttonText="검색" onClick={handleSearch} />
+              <SearchInput
+                  placeholder="검색어를 입력하세요"
+                  value={search} 
+                  onChange={handleSearchInputChange}
+              />
+              <ButtonMain buttonText="검색" onClick={handleSearch}/>
             </SearchContainer>
 
             </HomeExplain>
